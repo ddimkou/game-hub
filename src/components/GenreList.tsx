09c97eb253d91b320eca1fce5f55d6f2199f5981
ddimkou <ddimkou@outlook.com>
2023-07-28 11:22:32 +0300

    getch game genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useGenres from "../hooks/useGenres";
+import { list } from "@chakra-ui/react";
+
+const GenreList = () => {
+  const { genres } = useGenres();
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
