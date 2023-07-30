5690b0362a495c388db3be1568ae92a474d15cf5
ddimkou <ddimkou@outlook.com>
2023-07-30 23:32:07 +0300

    displaying genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} p={10} spacing={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} p={10} spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
